585edb4b75f77e796a3e896b076e268b22b537a9
Mihnea <m@mihneaparvanu.ro>
2025-06-11 23:59:51 +0300

    Initial Commit

---
+++ Color Generator/Color_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_GeneratorApp.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Color_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
