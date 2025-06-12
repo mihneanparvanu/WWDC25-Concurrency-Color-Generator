150ef0eaa92b8e6bdfc4bfea3ed55b9a0e5cf52c
Mihnea <m@mihneaparvanu.ro>
2025-06-13 00:14:47 +0300

    Clean-up and file organization before SwiftData

---
+++ Color Generator/Core/Models/ProcessedImage.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 12.06.2025.
+//
+
+import SwiftData
+import SwiftUI
+
+@Model
+class ProcessedImage {
+	
+}
---
+++ Color Generator/UI/Views/ExtractedColorsView.swift
@@ -0,0 +1,20 @@
+//
+//  ExtractedColorsView.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 12.06.2025.
+//
+
+import SwiftUI
+
+struct ExtractedColorsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+#Preview {
+    ExtractedColorsView()
+}
