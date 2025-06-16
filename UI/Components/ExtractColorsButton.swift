658da62c04510b46463a1cb9909cb57a5cf414dd
Mihnea <m@mihneaparvanu.ro>
2025-06-17 00:06:04 +0300

    Blur animation on button implemented

---
+++ UI/Components/ExtractColorsButton.swift
@@ -0,0 +1,18 @@
+//
+//  ExtractColorsButton.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 16.06.2025.
+//
+
+import SwiftUI
+
+struct ExtractColorsButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtractColorsButton()
+}
