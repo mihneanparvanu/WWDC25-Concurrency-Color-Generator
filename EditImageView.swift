c008c1585290660560eb8fdee4d4bcfe2e3012b4
Mihnea <m@mihneaparvanu.ro>
2025-06-18 17:21:24 +0300

    Starting rearcitecture

---
+++ EditImageView.swift
@@ -0,0 +1,18 @@
+//
+//  EditSheetView.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 18.06.2025.
+//
+
+import SwiftUI
+
+struct EditImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditImageView()
+}
