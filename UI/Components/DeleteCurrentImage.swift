0057910fea7e5a93e1a26e8a29226e4b54cda5b3
Mihnea <m@mihneaparvanu.ro>
2025-06-18 18:21:33 +0300

    Impelemented UI for editing selected image

--- UI/Components/DeleteCurrentImage.swift
+++ UI/Components/DeleteCurrentImage.swift
@@ -1,5 +1,5 @@
 //
-//  EditSheetView.swift
+//  DeleteCurrentImage.swift
 //  Color Generator
 //
 //  Created by Mihnea Nicolae Pârvanu on 18.06.2025.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct EditImageView: View {
+struct DeleteCurrentImage: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    EditImageView()
+    DeleteCurrentImage()
 }
