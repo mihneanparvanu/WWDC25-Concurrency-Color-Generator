cb1ee8cf808d7cf76661eef44b5f11aea79c5f42
Mihnea <m@mihneaparvanu.ro>
2025-06-27 22:47:24 +0300

    Implemented service for color extraction

---
+++ Core/Services/ColorExtractionService.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtractionService.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 27.06.2025.
+//
+
+import Foundation
---
+++ Utilities/Protocols/ColorExtractor.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtractor.swift
+//  Color Generator
+//
+//  Created by Mihnea Nicolae Pârvanu on 27.06.2025.
+//
+
+import Foundation
